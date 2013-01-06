cc1e5ec97389087ad3b88fa82f9c9df2b16a85de
mxiongmiao <mxiongmiao@gmail.com>
2013-01-06 15:49:00 +0800

    added memory data along with code optimized

--- shell/server.sh
+++ shell/server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+cpu=`cat /proc/cpuinfo | grep -m 1 "model name" | cut -d: -f2 | sed 's/^[ ]*//; s/[ ]*$//'`
 cpuNum=`cat /proc/cpuinfo | grep -c "model name"`
-cpuName=`cat /proc/cpuinfo | grep -m 1 "model name" | cut -d: -f2 | sed 's/^[ ]*//; s/[ ]*$//'`
+memTotal=`cat /proc/meminfo | grep "MemTotal:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
+memFree=`cat /proc/meminfo | grep "MemFree:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
 
-echo "cpuNum:$cpuNum"
-echo "cpuName:$cpuName"
+echo "cpu: $cpu"
+echo "cpuNum: $cpuNum"
+echo "mem: $memFree/$memTotal"
