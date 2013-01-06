cccad776112cda65591c25690fa9ed2e080f9108
mxiongmiao <mxiongmiao@gmail.com>
2013-01-06 17:40:19 +0800

    update server.sh , replace cat with more

--- shell/server.sh
+++ shell/server.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-cpu=`cat /proc/cpuinfo | grep -m 1 "model name" | cut -d: -f2 | sed 's/^[ ]*//; s/[ ]*$//'`
-cpuNum=`cat /proc/cpuinfo | grep -c "model name"`
-memTotal=`cat /proc/meminfo | grep "MemTotal:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
-memFree=`cat /proc/meminfo | grep "MemFree:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
+cpu=`more /proc/cpuinfo | grep -m 1 "model name" | cut -d: -f2 | sed 's/^[ ]*//; s/[ ]*$//'`
+cpuNum=`more /proc/cpuinfo | grep -c "model name"`
+memTotal=`more /proc/meminfo | grep "MemTotal:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
+memFree=`more /proc/meminfo | grep "MemFree:" | cut -d: -f2 | sed 's/^[ ]*//;s/[ ]*$//'`
+uptime=`uptime | sed 's/^[ ]*//; s/[ ]*$//'`
 
-echo "cpu: $cpu"
-echo "cpuNum: $cpuNum"
-echo "mem: $memFree/$memTotal"
+echo "cpu:"
+echo "$cpu"
+echo "cpuNum:"
+echo "$cpuNum"
+echo "mem:"
+echo "$memFree/$memTotal"
+echo "uptime:"
+echo "$uptime"
